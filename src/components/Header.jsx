db0c936704cb020c3d84996b5f777092b8c524cd
mizomizo1 <100689927+mizomizo1@users.noreply.github.com>
2025-09-26 16:55:15 +0900

    ProfileCardとIconButtonColorsコンポーネントを削除し、OutlinedCardコンポーネントを追加。Headerコンポーネントにログインボタンのクリックハンドラを追加。

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,11 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
+
 export default function ButtonAppBar() {
+  const handleLoginClick=()=>{
+    console.log('Login ボタンが押されました');
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -24,7 +28,7 @@ export default function ButtonAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             GenomeDataManager
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" onClick={handleLoginClick}>Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
